a8a2c78abcc4e0979676914005aeff97e8289c03
Meissa Dia <meissadia@gmail.com>
2018-03-26 16:16:05 -0700

    Refactoring. Reset gem ver.

--- lib/autofixer/commands/show_version.rb
+++ lib/autofixer/commands/show_version.rb
@@ -2,7 +2,7 @@ module DK
   class Idable
     def show_version(opts)
       return unless opts.include?('-v')
-      puts "\ntumblr_autofixer v0.0.3"
+      puts "\ntumblr_autofixer v0.0.1"
       puts
       exit(0)
     end
